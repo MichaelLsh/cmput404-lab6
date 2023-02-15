3b4354f30617539351f78db4205e071c3c36a7d0
MichaelLsh <shihao8@DESKTOP-JKGE86U.localdomain>
2023-02-14 17:50:52 -0700

    framework modified

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import django_on_heroku # top of the file
 from pathlib import Path
 import os
+import rest_framework
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -33,7 +34,6 @@ ALLOWED_HOSTS = []
 
 # Build-in frameworks
 INSTALLED_APPS = [
-    'rest_framework'
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'rest_framework',
+    'polls',
     'agenda.apps.AgendaConfig',
 ]
 
